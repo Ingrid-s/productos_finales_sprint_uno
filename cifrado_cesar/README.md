000f418acb60f1e92bf8ff97ba2c9e2937f9c0df
ingridsag <starlling@hotmail.com>
2017-10-30 11:07:14 -0600

    avances

---
+++ cifrado_cesar/README.md
@@ -0,0 +1,12 @@
+# PSEUDOCODIGO
+El programa cifrado cesar tiene por objetivo, cifrar y descifrar un mensaje.
+
+-Crear una web que pida una frase al usuario, con promt
+-validar que el mensaje no contenga campos vacios o números.
+-si es una frase valida, guardarla en una variable y -convertirla a minusculas.
+- de lo contrario, lanzar una alerta al usuario indicando que no puede poner una frase con espacios o números.
+-aplicar la formula para cifrar
+-mostrar el cifrado
+-aplicar la fórmula para descifrar
+-mostrar la frase descifrada.
+![cifrado_cesar] (https://www.lucidchart.com/documents/view/15b2cd27-1392-4cf8-bddd-d8d8216289ae)
---
+++ cifrado_cesar/js/app.js
@@ -0,0 +1,29 @@
+//pedir una frase al usuario por medio de un promt
+//prompt("Ingresa una frase");
+var str = 'hola';
+var phrase = console.log (str);
+//ar long = console.log(str.length);
+
+/*aquí el programa debe tener un if que valide si la frase proporcionada es válida, de lo contrario debe lanzar una alerta que diga que se debe ingresar una frase sin espacios, ni números.
+*/
+
+//alert('¡Ups, debes ingresar una frase sin espacios ni números!');
+
+/*se como hacer el cifrado pero no se como es la forma correcta de agrupar los elementos para que se cifren y decifren todos en pocas líneas de codigo.
+*/
+
+//var cipher = frase.charCodeAt(frase, i[0]));.....
+
+//'hola'.charCodeAt(0);//->104
+//'hola'.charCodeAt(1);//->111
+//'hola'.charCodeAt(2);//->108
+//'hola'.charCodeAt(3);//->97
+
+
+//var decipher = frase.charCodeAt(frase, i[0]));.....
+
+//String.fromCharCode(104);
+/*String.fromCharCode(111)
+String.fromCharCode(108)
+String.fromCharCode(97);
+*/
